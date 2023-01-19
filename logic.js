542012b3ab465fabad81972f4f4d6e1ddab8f420
Riccardo <gabelliniriccardo.94@gmail.com>
2023-01-19 11:39:54 +0100

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,8 +1,8 @@
 let count = 0;
 let lastBallColor = null;
 let balls = [];
-
-let canvas = document.getElementsByTagName("canvas")[0];
+let intervalTimeout;
+let canvas = document.querySelector("canvas");
 
 function setCanvasSize() {
     canvas.width = window.innerWidth - (window.innerWidth > 500 ? 100 : 10);
@@ -66,14 +66,54 @@ function draw() {
     canvas.style.borderColor = lastBallColor;
 }
 
-function addANewBall() {
+function addANewBall(x, y, dimension) {
     canvas = document.getElementsByTagName("canvas")[0];
     const color = '#' + ((1 << 24) * Math.random() | 0).toString(16);
-    const dimension = Math.random() * 100;
+    dimension = dimension ? dimension : Math.random() * 100;
     const speed_x = Math.round(Math.random() * 4);
     const speed_y = Math.round(Math.random() * 4);
-    balls.push(new Ball(canvas.width / 2, canvas.height / 2, color, dimension, speed_x, speed_y));
+    balls.push(new Ball(x ? x : canvas.width / 2, y ? y : canvas.height / 2, color, dimension, speed_x, speed_y));
     document.getElementById("ballNumberText").textContent = `There are ${balls.length} balls`
+}
+
+function changeInterval() {
+    clearInterval(intervalTimeout)
+    const interval = document.getElementById("interval").value
+    addANewBall(undefined, undefined, undefined);
+    intervalTimeout = setInterval(() => addANewBall(undefined, undefined, undefined), interval)
+}
+
+function reset() {
+    clearInterval(intervalTimeout)
+    balls = []
+    draw()
+}
+
+document.getElementById("interval").value = 1000;
+
+
+canvas.addEventListener('mousedown', function (e) {
+    addBallInPosition(canvas, e)
+})
+
+function addBallInPosition(canvas, event) {
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+    let radius = Math.random() * 100;
+    if (x + radius * 2 > canvas.width) {
+        radius = (canvas.width - x) / 2;
+    }
+    if (x - radius < 0) {
+        radius = x / 2
+    }
+    if (y + radius * 2 > canvas.height) {
+        radius = (canvas.height - y) / 2;
+    }
+    if (y - radius < 0) {
+        radius = y / 2
+    }
+    addANewBall(x, y, radius)
 }
 
 draw();
